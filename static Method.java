6eede95e2fdd36a1975e16121ee761a0b0b159fc
Asmitha M <108321537+Asmitha20@users.noreply.github.com>
2023-08-16 09:33:38 +0530

    Create static Method.java

---
+++ static Method.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+    static int a = 20;
+	public static void main(String[] args) {
+		System.out.println(a);
+		Main.display();
+	}
+	static void display(){
+	    System.out.println("Hello");
+	}
+}
